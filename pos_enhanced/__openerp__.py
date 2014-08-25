d8d037d0d605eb1e570d160b9d4d61ceceef8267
Abegail G Sanchez <abesanchez08@gmail.com>
2014-08-25 16:57:42 +0800

    Point Of Sale Enhancement

---
+++ pos_enhanced/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name':'Point of Sale Enhanced',
+    'version':'1.0',
+    'author':'OpenERP SA & Abegail G. Sanchez',
+    'category':'Generic Modules/Others',
+    'description':"""Point of Sale Order amount total auto calculation""",
+    'depends':['web','point_of_sale'],
+    'update_xml':[],
+    'js':['static/src/js/pos_enhanced.js'],
+    'auto_install':False,
+    'application':False,
+    'installable':True,
+}
